df9dc2c9c6d3e6144c2d981c03c1db05eba2d9dc
Gabril <62163345+Gabril0@users.noreply.github.com>
2023-04-16 23:15:40 -0300

    Add files via upload

--- Site de Vendas/JavaScript/script.js
+++ Site de Vendas/JavaScript/script.js
@@ -1,23 +1,14 @@
-const images = document.querySelectorAll('.carousel img');
-const prevButton = document.querySelector('.carousel .prev-button');
-const nextButton = document.querySelector('.carousel .next-button');
-let currentImageIndex = 0;
-
-function showImage(index) {
-    if (images && images.length > 0) {
-      images.forEach(image => image.classList.remove('active'));
-      images[index].classList.add('active');
-    }
-  }
-
-showImage(currentImageIndex);
-
-prevButton.addEventListener('click', () => {
-  currentImageIndex = (currentImageIndex - 1 + images.length) % images.length;
-  showImage(currentImageIndex);
-});
-
-nextButton.addEventListener('click', () => {
-  currentImageIndex = (currentImageIndex + 1) % images.length;
-  showImage(currentImageIndex);
-});
+document.addEventListener("DOMContentLoaded",()=>{
+  let img1 = document.querySelector(".imagem1");
+  let img2 = document.querySelector(".imagem2");
+  let botaoDir = document.querySelector(".rightButton");
+  let botaoEsq = document.querySelector(".leftButton");
+  botaoDir.addEventListener("click",()=>{
+    img1.style.display = "none";
+    img2.style.display = "inline-block";
+  })
+  botaoEsq.addEventListener("click",()=>{
+    img2.style.display = "none";
+    img1.style.display = "inline-block";
+  })
+})
