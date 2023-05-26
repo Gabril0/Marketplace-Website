be7dbe5fceabd5ea503339b21b1ba07911d99554
ThurFerreira <arthurfsilva110@gmail.com>
2023-05-25 21:05:15 -0300

    commit seila

--- Site de Vendas/php/isLogged.php
+++ Site de Vendas/php/isLogged.php
@@ -1,7 +1,7 @@
 <?php
     session_start();
     
-    if(!isset($_SESSION['emailAnunciante'] && isset($_SESSION['codAnunciante']))){
-        header("location: html/loginPage.html" )
+    if(!isset($_SESSION['emailAnunciante']) && isset($_SESSION['codAnunciante'])){
+        header("Location: html/loginPage.html" );
     }
 ?>
