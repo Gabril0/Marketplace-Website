0c83091f338166acca53fc0f2149f2bb91df4f1a
ThurFerreira <arthurfsilva110@gmail.com>
2023-06-05 17:01:46 -0300

    cadastra anuncio funcionando

--- Site de Vendas/php/anuncio.php
+++ Site de Vendas/php/anuncio.php
@@ -1,56 +1,56 @@
 <?php
-    require "../conexaoMysql.php";
-    require "isLogged.php";
+require "../conexaoMysql.php";
 
-    session_start();
-    //verifyLoggin();
+//session_start();
 
-    $database = mysqlConnect();
+$database = mysqlConnect();
 
-    $titulo = $_POST["titulo"] ?? "";
-    $descricao = $_POST["descricao"] ?? "";
-    $preco = $_POST["preco"] ?? "";
-    $dataHora = strval(date('Y-m-d, H:i'));
-    $CEP = $_POST["CEP"] ?? "";
-    $bairro = $_POST["bairro"] ?? "";
-    $cidade = $_POST["cidade"] ?? "";
-    $estado = $_POST["estadoProduto"] ?? "";
-    $codCategoria = $_POST["codCategoria"] ?? "" ;
-    $codAnunciante = 1;
-    // $codAnunciante = $_SESSION['codAnunciante'];
-    //$img = $_POST["img"] ?? "";
-    
-    try{
+$titulo = $_POST["titulo"] ?? "";
+$descricao = $_POST["descricao"] ?? "";
+$preco = $_POST["preco"] ?? "";
+$dataHora = strval(date('Y-m-d, H:i'));
+$CEP = $_POST["CEP"] ?? "";
+$bairro = $_POST["bairro"] ?? "";
+$cidade = $_POST["cidade"] ?? "";
+$estado = $_POST["estadoProduto"] ?? "";
+$codCategoria = $_POST["codCategoria"] ?? "";
+$codAnunciante = 2;
+// $codAnunciante = $_SESSION['codAnunciante'];
+//$img = $_POST["img"] ?? "";
 
-        $database->beginTransaction();//inicia transação com o banco
+try {
 
-        $query = <<<SQL
+    $database->beginTransaction(); //inicia transação com o banco
+
+    $query = <<<SQL
             INSERT INTO Anuncio (titulo, descricao, preco, dataHora, cep, bairro, cidade, estado, codCategoria, codAnunciante)
             VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
             SQL;
 
-        // $queryIMG = <<<SQL
-        //     INSERT INTO Foto img
-        //     VALUES ?
-        //     SQL;
+    // $queryIMG = <<<SQL
+    //     INSERT INTO Foto img
+    //     VALUES ?
+    //     SQL;
 
-        echo  "<script>alert('enviado com Sucesso!);</script>";
-        echo ' titulo: '. $titulo, '| descricao: ' . $descricao, '| preco: ' . $preco, '| datahora: ' . $dataHora, '| cep: ' . $CEP, '| bairro: ' . $bairro, '| cidade: ' . $cidade, '| estado: ' . $estado, '| cod1: '. $codCategoria, '| cod2: ' . $codAnunciante;
-        $stmt = $database->prepare($query);
-        if (!$stmt->execute([$titulo, $descricao, $preco, $dataHora, $CEP, $bairro, $cidade, $estado, intval($codCategoria), $codAnunciante]))
-            throw new Exception('Falha de inserção de produto');
+    echo "<script>alert('enviado com Sucesso!);</script>";
+    echo ' titulo: ' . $titulo, '| descricao: ' . $descricao, '| preco: ' . $preco, '| datahora: ' . $dataHora, '| cep: ' . $CEP, '| bairro: ' . $bairro, '| cidade: ' . $cidade, '| estado: ' . $estado, '| cod1: ' . $codCategoria, '| cod2: ' . $codAnunciante;
+    $stmt = $database->prepare($query);
+    if (!$stmt->execute([$titulo, $descricao, $preco, $dataHora, $CEP, $bairro, $cidade, $estado, intval($codCategoria), $codAnunciante]))
+        throw new Exception('Falha de inserção de produto');
 
-        // $smt = $database->prepare($queryIMG);
-        // if (!$smt->execute([$img]));
-        //     throw new Exception('Falha de inserção da foto');
-        
-        $database->commit();//confirma as transações feitas anteriormente com o banco
-    
-        header("Content-Type: application/json");
-        echo json_encode("obrigado.html");
+    // $smt = $database->prepare($queryIMG);
+    // if (!$smt->execute([$img]));
+    //     throw new Exception('Falha de inserção da foto');
 
-    }catch(Exception $e) {
-        $database->rollBack();//nega e volta atras da transações feitas
-        exit("Falha ao cadastrar anuncio" . $e->getMessage());
-    }
+    $database->commit(); //confirma as transações feitas anteriormente com o banco
+
+    header("location: ../obrigado.html");
+    exit();
+    // header("Content-Type: application/json");
+    // echo json_encode("obrigado.html");
+
+} catch (Exception $e) {
+    $database->rollBack(); //nega e volta atras da transações feitas
+    exit("Falha ao cadastrar anuncio" . $e->getMessage());
+}
 ?>
